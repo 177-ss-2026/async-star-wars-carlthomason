2bdf77bc5ee315d75bbc61e18b5ffa03dc0aacf5
Carl Thomason <sappers2016@gmail.com>
2026-05-06 20:52:34 -0500

    feat: add logic to tryit2

--- src/main.js
+++ src/main.js
@@ -1,7 +1,23 @@
-async function getPlanet(id) {
+//Try It 1: etch a Planet with await
+/* async function getPlanet(id) {
   const resp = await fetch(`https://swapi.info/api/planets/${id}`);
   const data = await resp.json();
   console.info(data);
 }
 
-getPlanet(1);
+getPlanet(1); */
+
+//Try It 2: Check resp.ok
+async function getPlanet(id) {
+  const resp = await fetch(`https://swapi.info/api/planets/${id}`);
+
+  if (!resp.ok) {
+    console.error(`Request failed: ${resp.status}`);
+    return;
+  }
+
+  const data = await resp.json();
+  console.info(data);
+}
+
+getPlanet(999); // Bad ID — watch the console
